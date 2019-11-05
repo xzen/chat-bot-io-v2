bbda1f2933ca7267126eb900a2e6af66c7d54ef4
cvimard <cvimard@socialobjects.co>
2019-11-05 15:50:38 +0100

    style(index.js): add color h1 and h2

--- src/index.js
+++ src/index.js
@@ -6,23 +6,27 @@ class HelloWorld {
    * @constructor
    * {String} say
    */
-  constructor(say) {
+  constructor(say = 'Hello World') {
     this.say = say;
   }
 
   /**
    * speak
+   * {String} say
    */
   speak(say = this.say) {
     const el = document.querySelector('#app');
     const elH1 = document.createElement('h1');
+    const elH2 = document.createElement('h2');
 
     elH1.textContent = say;
+    elH2.textContent = say;
     el.appendChild(elH1);
+    el.appendChild(elH2);
   }
 }
 
-const helloWorld = new HelloWorld('Bonjour le monde');
+const helloWorld = new HelloWorld();
 
 helloWorld.speak();
-helloWorld.speak('Hello World');
+helloWorld.speak('Bonjour le monde');
