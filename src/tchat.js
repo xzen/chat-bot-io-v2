eff88dd1e57b395a294f015ea37e88f18f95bc83
cvimard <cvimard@socialobjects.co>
2019-11-25 12:06:41 +0100

    feat(tchat): add message received with item data

--- src/tchat.js
+++ src/tchat.js
@@ -1,6 +1,40 @@
 class Tchat {
   render() {
+    const tchatMessages = document.querySelector('.tchat--messages');
+    const item = {
+      avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/theory-1552510536.jpg?crop=0.501xw:1.00xh;0.0173xw,0&resize=480:*',
+      name: 'Sansa',
+      message: 'Bonjour Cyril',
+    };
+
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+
     this.sendValue();
+  }
+
+  /**
+   * tpl message received
+   * {Object} item
+   */
+  tplMessageReceived(item) {
+    const { avatar, name, message } = item;
+    const date = new Date();
+
+    return `
+      <div class="row mb-1">
+        <div class="col-6">
+          <div class="card">
+            <h5 class="card-header">
+              <img src="${avatar}" width="50" alt="avatar" class="rounded-circle">
+            </h5>
+            <div class="card-body">
+              <h5 class="card-title">${message}</h5>
+              <p class="card-text">${name} | ${date.toLocaleString()}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-6"></div>
+      </div>`;
   }
 
   /**
