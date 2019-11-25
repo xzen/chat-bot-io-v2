996a6824454f3d88e19bd6e467e490c5a00342a9
cvimard <cvimard@socialobjects.co>
2019-11-25 12:25:17 +0100

    feat'(tchat): add contact template'

--- src/tchat.js
+++ src/tchat.js
@@ -1,27 +1,69 @@
 class Tchat {
+  /**
+   * render
+   */
   render() {
     const tchatMessages = document.querySelector('.tchat--messages');
+    const tchatContacts = document.querySelector('.tchat--contacts');
     const item = {
+      id: 'sansa-123',
       avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/theory-1552510536.jpg?crop=0.501xw:1.00xh;0.0173xw,0&resize=480:*',
       name: 'Sansa',
       message: 'Bonjour Cyril',
     };
 
     tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+
+    tchatContacts.innerHTML += this.tplContact(item);
 
     this.sendValue();
   }
 
   /**
-   * tpl message received
-   * {Object} item
+   * tpl contact
+   * @param {Object} item
+   * @param {String} item.id
+   * @param {String} item.avatar
+   * @param {String} item.name
+   * @param {String} item.message
    */
-  tplMessageReceived(item) {
-    const { avatar, name, message } = item;
-    const date = new Date();
+  tplContact(item) {
+    const { id, avatar, name } = item;
+    const count = document.querySelectorAll(`.${id}`).length;
 
     return `
-      <div class="row mb-1">
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <img src="${avatar}" width="50" alt="avatar" class="rounded-circle">
+        <h5 class="display-5 mt-2">${name}</h5>
+        <span class="badge badge-primary badge-pill">${count}</span>
+      </li>`;
+  }
+
+  /**
+   * tpl message received
+   * @param {Object} item
+   * @param {String} item.id
+   * @param {String} item.avatar
+   * @param {String} item.name
+   * @param {String} item.message
+   */
+  tplMessageReceived(item) {
+    const date = new Date();
+    const {
+      avatar,
+      id,
+      name,
+      message,
+    } = item;
+
+    return `
+      <div class="${id} row mb-1">
         <div class="col-6">
           <div class="card">
             <h5 class="card-header">
@@ -39,7 +81,7 @@ class Tchat {
 
   /**
    * tpl message send
-   * {String} message
+   * @param {String} message
    */
   tplMessageSend(message) {
     const date = new Date();
