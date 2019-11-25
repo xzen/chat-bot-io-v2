48c7b7c21e17835b27d91f502f259f558a96dd87
cvimard <cvimard@socialobjects.co>
2019-11-25 11:38:31 +0100

    feat(tchat): display message send

--- src/tchat.js
+++ src/tchat.js
@@ -4,15 +4,39 @@ class Tchat {
   }
 
   /**
+   * tpl message send
+   * {String} message
+   */
+  tplMessageSend(message) {
+    const date = new Date();
+    return `
+      <div class="row mb-1">
+        <div class="col-6"></div>
+        <div class="col-6">
+          <div class="card">
+            <h5 class="card-header">
+              <img src="https://png.pngtree.com/svg/20161212/f93e57629c.svg" width="50" alt="avatar" class="rounded-circle">
+            </h5>
+            <div class="card-body">
+              <h5 class="card-title">${message}</h5>
+              <p class="card-text">${date.toLocaleString()}</p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  /**
    * send value
    */
   sendValue() {
+    const tchatMessages = document.querySelector('.tchat--messages');
     const elBtn = document.querySelector('.tchat--button');
 
     elBtn.addEventListener('click', (e) => {
       e.preventDefault();
 
-      console.log(this.getInputValue());
+      tchatMessages.innerHTML += this.tplMessageSend(this.getInputValue());
     });
   }
 
